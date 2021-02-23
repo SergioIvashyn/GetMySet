c94439a930c83ad93faaf48c82417ce4acbb03ac
SergioIvashyn <ivashimsergio@gmail.com>
2021-02-23 11:45:36 +0200

    Reformating and fix setting import error

--- demo/urls.py
+++ demo/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, re_path, include
 from django.conf import settings
 from django.views.generic import TemplateView
 
-
 admin.site.site_header = 'Admin console'
 
 urlpatterns = [
@@ -14,20 +13,21 @@ urlpatterns = [
     path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
 ]
 
-
 # For debug mode only
 if settings.DEBUG:
     # Turn on debug toolbar
     import debug_toolbar
+
     urlpatterns += [
         re_path(r'^__debug__/', include(debug_toolbar.urls)),
     ]
     # Serve media files via Django
     import django.views.static
+
     urlpatterns += [
         re_path(r'media/(?P<path>.*)$',
-            django.views.static.serve, {
-                'document_root': settings.MEDIA_ROOT,
-                'show_indexes': True,
-            }),
+                django.views.static.serve, {
+                    'document_root': settings.MEDIA_ROOT,
+                    'show_indexes': True,
+                }),
     ]
