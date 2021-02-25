07b1584a1ff5ad8a8088a46fc49a40ac785f118b
SergioIvashyn <ivashimsergio@gmail.com>
2021-02-25 16:40:47 +0200

    Customize Project resource import

--- apps/project/resource.py
+++ apps/project/resource.py
@@ -4,6 +4,15 @@ from apps.project.models import Project
 
 
 class ProjectResource(resources.ModelResource):
-
     class Meta:
         model = Project
+        fields = ('name', 'url', 'description', 'notes', 'is_private')
+
+    def import_row(self, row, *args, **kwargs):
+        row_result = super(ProjectResource, self).import_row(row, *args, **kwargs)
+        return row_result
+
+    def import_data(self, dataset, *args, **kwargs):
+        result = super(ProjectResource, self).import_data(dataset, *args, **kwargs)
+        print(result)
+        return result
