876d778480e99e730ae5646afbb3ec8fd97ddc09
SergioIvashyn <ivashimsergio@gmail.com>
2021-02-26 19:32:09 +0200

    Pagination for projects page

--- apps/project/views.py
+++ apps/project/views.py
@@ -11,12 +11,13 @@ def private_projects(request):
     industries = Industry.objects.all()
     technologies = Technology.objects.all()
     page_number = request.GET.get('page', 1)
+    print(page_number)
     qs = Project.objects.private()
-    paginator = Paginator(qs, 25)
+    paginator = Paginator(qs, 3)
     try:
         page_obj = paginator.page(page_number)
-    except PageNotAnInteger:
-        page_obj = paginator.page(1)
+    # except PageNotAnInteger:
+    #     page_obj = paginator.page(1)
     except EmptyPage:
         page_obj = paginator.page(paginator.num_pages)
 
@@ -29,4 +30,5 @@ def public_projects(request):
     qs = Project.objects.public()
     paginator = Paginator(qs, 25)
     page_obj = paginator.get_page(page_number)
+    print(page_obj.has_other_pages())
     return render(request, 'project/projects.html', {'qs': page_obj})
