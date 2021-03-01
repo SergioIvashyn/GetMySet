ce7e009d848806164ac53971045ed510541baaac
SergioIvashyn <ivashimsergio@gmail.com>
2021-03-01 16:52:07 +0200

    Update base.py

--- demo/settings/base.py
+++ demo/settings/base.py
@@ -201,10 +201,10 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.auth_allowed',
     'social_core.pipeline.social_auth.social_user',
     'social_core.pipeline.user.get_username',
-    'social_core.pipeline.user.create_user',
+    'apps.accounts.services.custom_social_auth_pipeline.custom_create_user',
     'social_core.pipeline.social_auth.associate_user',
     'social_core.pipeline.social_auth.load_extra_data',
-    'apps.accounts.services.custom_user_details.custom_user_details',
+    'apps.accounts.services.custom_social_auth_pipeline.custom_user_details',
 )
 
 NO_DEFAULT_PROTECTED_USER_FIELDS = False
