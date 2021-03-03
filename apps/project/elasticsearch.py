c828750d3017e5332aea3e5aafefb2b02321d579
SergioIvashyn <ivashimsergio@gmail.com>
2021-03-03 14:00:09 +0200

    Change project model index

--- apps/project/elasticsearch.py
+++ apps/project/elasticsearch.py
@@ -1,3 +1,5 @@
+from django.db.models import QuerySet
+
 from apps.core.services.elasticsearch_api_base import ElasticSearchModelService
 from apps.core.models import Project
 
@@ -20,23 +22,30 @@ class ProjectElasticSearchModelService(ElasticSearchModelService):
                 "type": 'nested',
                 "properties": {
                     "pk": {"type": "integer"},
-                    "name": {"type": "text"}
+                    "name": {"type": "text"},
+                    "name_keyword": {"type": "keyword"},
                 }
             },
             "industries": {
                 "type": 'nested',
                 "properties": {
                     "pk": {"type": "integer"},
-                    "name": {"type": "text"}
+                    "name": {"type": "text"},
+                    "name_keyword": {"type": "keyword"},
                 }
             }
         }
     }
 
+    def get_query_set(self) -> QuerySet:
+        return self.model.objects.all().prefetch_related('technologies').prefetch_related('industries')
+
     def get_model_body(self, obj: Project) -> dict:
         return {
             "name": obj.name,
             "description": obj.description,
-            "technologies": [{"pk": elem.pk, "name": elem.name} for elem in obj.technologies.all()],
-            "industries": [{"pk": elem.pk, "name": elem.name} for elem in obj.industries.all()]
+            "technologies": [{"pk": elem.pk, "name": elem.name, "name_keyword": elem.name}
+                             for elem in obj.technologies.all()],
+            "industries": [{"pk": elem.pk, "name": elem.name, "name_keyword": elem.name}
+                           for elem in obj.industries.all()]
         }
