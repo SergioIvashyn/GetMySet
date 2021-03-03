67daf14110fa00137b92c6e314c2851fa18ae3af
SergioIvashyn <ivashimsergio@gmail.com>
2021-03-03 23:48:45 +0200

    Remove validation into project.url field

--- apps/project/admin.py
+++ apps/project/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django import forms
+from django.forms import ValidationError
+from import_export.formats.base_formats import CSV
 from import_export.forms import ImportForm, ConfirmImportForm
-
+from django.utils.translation import gettext_lazy as _
 from apps.accounts.models import User
 from apps.core.models import Project
 from apps.project.resource import ProjectResource
@@ -13,6 +15,20 @@ from import_export.admin import ImportExportModelAdmin
 class ProjectImportForm(ImportForm):
     user = forms.ModelChoiceField(queryset=User.objects.all(), required=False)
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def clean_import_file(self):
+        if self.cleaned_data['import_file'].content_type not in {'application/vnd.ms-excel', 'text/csv'}:
+            raise ValidationError(_('Current file is not CSV'))
+        return self.cleaned_data['import_file']
+
+    def clean(self):
+        cleaned_data = super(ProjectImportForm, self).clean()
+        # print(self.cleaned_data['import_file'].name)
+        # print(dir(self.cleaned_data['import_file']))
+        return cleaned_data
+
 
 class ProjectConfirmImportForm(ConfirmImportForm):
     user = forms.ModelChoiceField(queryset=User.objects.all(), widget=forms.HiddenInput(), required=False)
@@ -20,6 +36,9 @@ class ProjectConfirmImportForm(ConfirmImportForm):
 
 class ProjectAdmin(ImportExportModelAdmin):
     resource_class = ProjectResource
+    from_encoding = "utf-8"
+    formats = (CSV,)
+    import_template_name = 'import_export.html'
 
     def get_resource_kwargs(self, request, *args, **kwargs):
         result = super(ProjectAdmin, self).get_resource_kwargs(request, *args, **kwargs)
